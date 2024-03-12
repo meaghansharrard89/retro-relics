60744e7785f297f81169bccf7bea323aca81c888
Meaghan <meaghan.sharrard89@gmail.com>
2024-03-11 23:04:59 -0700

    fix footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,13 +4,13 @@ function Footer() {
   return (
     <>
       <footer className="bg-dark-accent-light text-center text-darker-accent lg:text-left min-h-screen-30">
-        <div className="mx-2 md:mx-6 py-2 md:py-0">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <div className="text-center mx-2 md:mx-6 py-2 md:py-0">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
             <div className="mb-4 md:mb-0">
               <h6 className="mb-2 font-semibold uppercase text-black">
-                Contact
+                Contact: info@example.com
               </h6>
-              <p className="mb-2 flex items-center justify-center md:justify-start text-black">
+              {/* <p className="mb-2 flex items-center justify-center md:justify-start text-black">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -21,26 +21,26 @@ function Footer() {
                   <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                 </svg>
                 info@example.com
-              </p>
+              </p> */}
             </div>
             <div className="mb-4 md:mb-0">
               <h6 className="mb-2 font-semibold uppercase text-black">
-                © 2023 Copyright:
+                © 2023 Copyright: Retro Revival
               </h6>
-              <p
+              {/* <p
                 className="mb-2 flex items-center justify-center md:justify-start text-black"
                 href="https://tw-elements.com/"
               >
                 Retro Revival
-              </p>
+              </p> */}
             </div>
-            <div className="mb-4 md:mb-0 flex justify-center md:justify-start items-center">
-              <img
-                src="https://i.ibb.co/w4CTq0R/updated.png"
-                alt="Your Image Alt Text"
-                className="mx-auto md:mx-0 max-h-10 max-w-10"
-              />
-            </div>
+            {/* <div className="mb-4 md:mb-0 flex justify-center md:justify-start"> */}
+            {/* <img
+              src="https://i.ibb.co/w4CTq0R/updated.png"
+              alt="Your Image Alt Text"
+              className="mx-auto md:mx-0 max-h-10 max-w-10"
+            /> */}
+            {/* </div> */}
           </div>
         </div>
       </footer>
